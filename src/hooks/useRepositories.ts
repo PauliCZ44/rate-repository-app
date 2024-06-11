54d5b5e094ed9e1205cf1bfe3af7544a58ccf4fb
Pavel Šťastný <stastnyp@seznam.cz>
2024-06-11 11:04:41 +0200

    Exercise 10.11: fetching repositories with Apollo Client

--- src/hooks/useRepositories.ts
+++ src/hooks/useRepositories.ts
@@ -3,37 +3,14 @@ import { useEffect, useState } from 'react'
 import { GET_REPOSITORIES } from '../graphql/queries'
 
 export const useRepositories = () => {
-  const [repositories, setRepositories] = useState<any>()
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState(false)
-
-    const fetchRepositories = async () => {
-        const { loading, error, data } = useQuery(GET_REPOSITORIES);
-        
-        
-    setLoading(true)
-    try {
-      const response = await fetch('http://192.168.0.136:5000/api/repositories')
-      const json = await response.json()
-
-      console.log(json)
-
-      setRepositories(json)
-    } catch (error) {
-      console.error(error)
-      setError(true)
-    }
-    setLoading(false)
-  }
-
-  useEffect(() => {
-    fetchRepositories()
-  }, [])
-
+  const { loading, error, data } = useQuery(GET_REPOSITORIES, {
+    fetchPolicy: 'cache-and-network'
+  })
+  console.log('data', data)
   // Get the nodes from the edges array
-  const repositoryNodes = repositories
-    ? repositories.edges.map((edge) => edge.node)
+  const repositories = data
+    ? data.repositories.edges.map((edge) => edge.node)
     : []
 
-  return { repositories: repositoryNodes, loading, error }
+  return { repositories, loading, error }
 }
